2bf335d876e6a8fed6cf26b9750c0b9902c2bb03
Jordan Kaspers <kaspers@esat-alumni.com>
2018-03-14 15:08:42 +0100

    Finding Screen Pixel Coordinates

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -14,13 +14,14 @@ UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
-public:
-  ATank* GetControlledTank() const;
 
+public:
   virtual void BeginPlay() override;
 	
   virtual void Tick(float DeltaTime) override;
+
+private:
+  ATank* GetControlledTank() const;
 
   // Start the tank moving the barrel so that a shot would hit where
   //the crosshair intersects the world
@@ -28,5 +29,13 @@ public:
 
   // Returns an OUT parameter, true if hit landscape
   bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+
+  UPROPERTY(EditAnywhere)
+  float CrossHairXLocation = 0.5f;
+
+  UPROPERTY(EditAnywhere)
+  float CrossHairYLocation = 0.33333f;
+
+private:
 	
 };
