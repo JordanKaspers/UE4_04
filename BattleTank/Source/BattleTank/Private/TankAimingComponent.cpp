1f5c6124e03dfb303811ce641a760648daab80a9
Jordan Kaspers <kaspers@esat-alumni.com>
2018-03-15 18:11:19 +0100

    Setting Up Projectiles

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -53,17 +53,9 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
     if(bHaveAimSolution)
     {
       auto AimDirection = OutLaunchVelocity.GetSafeNormal();
-      auto Time = GetWorld()->GetTimeSeconds();
-      UE_LOG(LogTemp, Warning, TEXT("%f: Aim solution found"), Time);
       MoveBarrelTowards(AimDirection);
     }
-    else
-    {
-      //  If no solution found, do nothing
-      auto Time = GetWorld()->GetTimeSeconds();
-      UE_LOG(LogTemp, Warning, TEXT("%f: No aim solution found"), Time);
-    }
-
+    //  If no solution found, do nothing
   }
 }
 
