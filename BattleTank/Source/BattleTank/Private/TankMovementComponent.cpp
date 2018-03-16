e6d06aa84c27dcac99399c2a1ab5e8d645414b3e
Jordan Kaspers <kaspers@esat-alumni.com>
2018-03-16 19:08:17 +0100

    Using BlueprintReadOnly

---
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankMovementComponent.h"
+#include "Engine/World.h"
+
+void UTankMovementComponent::IntendMoveForward(float Throw)
+{
+
+  auto Time = GetWorld()->GetTimeSeconds();
+  auto Name = GetName();
+  UE_LOG(LogTemp, Warning, TEXT("%f: Intend move forward thrown: %f"), Time, Throw);
+}
+
--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -5,9 +5,6 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-  auto Time = GetWorld()->GetTimeSeconds();
-  auto Name = GetName();
-  UE_LOG(LogTemp, Warning, TEXT("%f: %s throttle: %f"), Time, *Name, Throttle);
 
   // TODO Clamp actual throttle value so player can't over-drive
   auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
